4dff57795626c6e907e8f1cb44332391a20de705
Fernando Espinosa <fespinosa@ucdavis.edu>
2018-10-14 10:27:40 -0700

    small

--- midikeys_serial.py
+++ midikeys_serial.py
@@ -1,45 +1,33 @@
-import pygame 
-import pygame.midi
-from pygame.locals import *
+import logging
+import sys
+import time
 
-pygame.init()
-pygame.fastevent.init()
-event_get = pygame.fastevent.get
-event_post = pygame.fastevent.post
+from rtmidi.midiutil import open_midiinput
 
-pygame.midi.init()
-# print(pygame.midi.get_device_info(2))
-i = pygame.midi.Input(2)
-pygame.display.set_caption("Reading MIDI Keyboard Input")
-screen = pygame.display.set_mode((400,300), RESIZABLE, 32)
+log = logging.getLogger('midiin_poll')
+logging.basicConfig(level=logging.DEBUG)
 
-going = True
-while going:
+try:
+	midiin, port_name = open_midiinput(3)
+except (EOFError, KeyboardInterrupt):
+	sys.exit()
 
-        events = event_get()
-        print(i.poll())
-        for e in events:
-                if e.type in [QUIT]:
-                        going = False
-                if e.type in [KEYDOWN]:
-                        going = False
+print("Press Control-C to exit")
+try:
+	timer = time.time()
+	while True:
+		msg = midiin.get_message()
+		if msg:
+			message, deltatime = msg
+			timer += deltatime
+			print(port_name)
+			print(timer)
+			print(message)
 
-        if i.poll():
-                midi_events = i.read(10)
-
-                if int(midi_events[0][0][0]) in [224,225,226]:#Pitch Bender
-                        print(str(midi_events[0][0][2]))#right(0)  center(64)  left(124)
-                        
-                # print("full midi_events " + str(midi_events))
-                print("my midi note is " + str(midi_events[0][0][1]))
-                # convert them into pygame events.
-                midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
-
-                for m_e in midi_evs:
-                        event_post( m_e )
-
-print("exit button clicked.")
-i.close()
-pygame.midi.quit()
-pygame.quit()
-exit()
+		time.sleep(0.01)
+except (KeyboardInterrupt):
+	print('')
+finally:
+	print("Exit.")
+	midiin.close_port()
+	del midiin
